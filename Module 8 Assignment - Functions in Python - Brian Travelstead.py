3c533cc1b30d8b22974ce0768e1a65e06f6471dc
Btravelstead13 <btravelstead1@ivytech.edu>
2025-12-23 09:39:17 -0500

    module 8 upload

---
+++ Module 8 Assignment - Functions in Python - Brian Travelstead.py
@@ -0,0 +1,12 @@
+def greater_than(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+a = 10
+b = 6
+
+result = greater_than(a, b)
+
+print(f"The statement {a} is greater than {b} is {result}")
---
+++ square_program_example.py
@@ -0,0 +1,10 @@
+def square(x):
+    y = x * x
+    return y
+result = square(10)
+print(result)
+
+a = 10
+b = 6
+
+print(f"The square of {a} is {square(a)}")
